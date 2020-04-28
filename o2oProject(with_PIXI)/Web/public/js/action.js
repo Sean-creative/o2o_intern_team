0c4434e9484478acddfded29d0751570cbf4031c
kkpcs7042 <43942477+kkpcs7042@users.noreply.github.com>
2020-04-28 14:18:17 +0900

    Update action.js

--- o2oProject(with_PIXI)/Web/public/js/action.js
+++ o2oProject(with_PIXI)/Web/public/js/action.js
@@ -32,19 +32,19 @@ class Action {
     this.commands = {
       HI: function(data) {
         console.log("command : " + data.command);
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Hi_container.visible = true;
         that.scene.Fallback_container.visible = false;
         that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
         that.scene.Info_container.visible = false;
-//        that.scene.Infotel_container.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Hi_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Hi_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Hi_chip_Aog.texture = that.scene.Unclick_chip_Aog;
         that.scene.Hi_chip_Info.texture = that.scene.Unclick_chip_Info;
 
-
+        //welcome인텐트에서 위치가 변경된 오프젝트의 위치를 초기화
         that.scene.Hi_ottu.width = 0;
         that.scene.Hi_ottu.height = 0;
         that.scene.Hi_ottu.x = 50;
@@ -78,55 +78,52 @@ class Action {
         that.scene.Hi_bubble.x = 21;
         that.scene.Hi_bubble.y = -270;
 
+        //액션 재시작
         that.scene.Hi = 0;
 
       },
 
       GREETING: function(data) {
         console.log("command : " + data.command);
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Hi_container.visible = false;
         that.scene.Fallback_container.visible = false;
         that.scene.Greeting_container.visible = true;
         that.scene.Info_container.visible = false;
-  //      that.scene.Aog_container.visible = false;
-//        that.scene.Infotel_container.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Greeting_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Greeting_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Greeting_chip_Aog.texture = that.scene.Unclick_chip_Aog;
         that.scene.Greeting_chip_Info.texture = that.scene.Unclick_chip_Info;
 
+        //액션 재시작
         that.scene.Greeting = 0;
 
       },
 
       AOG: function(data) {
         console.log("command : " + data.command);
-/*        that.scene.Hi_container.visible = false;
-        that.scene.Fallback_container.visible = false;
-        that.scene.Greeting_container.visible = false;
-        that.scene.Aog_container.visible = true;
-        that.scene.Info_container.visible = false;
-        that.scene.Infotel_container.visible = false;
-*/
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Aog_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Aog_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Aog_chip_Aog.texture = that.scene.Unclick_chip_Aog;
         that.scene.Aog_chip_Info.texture = that.scene.Unclick_chip_Info;
 
+        //액션 재시작
         that.scene.hello = 0;
         that.scene.swing = 0;
       },
 
       INFO: function(data) {
         console.log("command : " + data.command);
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Hi_container.visible = false;
         that.scene.Fallback_container.visible = false;
         that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
         that.scene.Info_container.visible = true;
-  //      that.scene.Infotel_container.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Info_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Info_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Info_chip_Aog.texture = that.scene.Unclick_chip_Aog;
@@ -135,18 +132,19 @@ class Action {
 
       FALLBACK: function(data) {
         console.log("command : " + data.command);
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Hi_container.visible = false;
         that.scene.Fallback_container.visible = true;
         that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
         that.scene.Info_container.visible = false;
-//        that.scene.Infotel_container.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Fallback_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Fallback_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Fallback_chip_Aog.texture = that.scene.Unclick_chip_Aog;
         that.scene.Fallback_chip_Info.texture = that.scene.Unclick_chip_Info;
 
+        //액션 재시작
         that.scene.Fallback = 0;
         that.scene.time = 0;
 
@@ -155,11 +153,7 @@ class Action {
 
       ADDRESSDIV_INFO_OPTION: function(data) {
         console.log("command : " + data.command);
-//        that.scene.Hi_container.visible = false;
-//        that.scene.Fallback_container.visible = false;
-//        that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
-//        that.scene.Info_container.visible = false;
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Infotel_container.visible = true;
         that.scene.Infotel_prize.visible = false;
         that.scene.Infotel_address.visible = true;
@@ -173,6 +167,7 @@ class Action {
         that.scene.Infotel_phone.visible = false;
         that.scene.Infotel_email.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Infotel_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Infotel_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Infotel_chip_Aog.texture = that.scene.Unclick_chip_Aog;
@@ -181,11 +176,7 @@ class Action {
 
       HOMEPAGEDIV_INFO_OPTION: function(data) {
         console.log("command : " + data.command);
-//        that.scene.Hi_container.visible = false;
-//        that.scene.Fallback_container.visible = false;
-//        that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
-//        that.scene.Info_container.visible = false;
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Infotel_container.visible = true;
         that.scene.Infotel_prize.visible = false;
         that.scene.Infotel_address.visible = false;
@@ -199,6 +190,7 @@ class Action {
         that.scene.Infotel_phone.visible = false;
         that.scene.Infotel_email.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Infotel_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Infotel_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Infotel_chip_Aog.texture = that.scene.Unclick_chip_Aog;
@@ -207,11 +199,7 @@ class Action {
 
       CONTACTDIV_INFO_OPTION: function(data) {
         console.log("command : " + data.command);
-//        that.scene.Hi_container.visible = false;
-//        that.scene.Fallback_container.visible = false;
-//        that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
-//        that.scene.Info_container.visible = false;
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Infotel_container.visible = true;
         that.scene.Infotel_prize.visible = false;
         that.scene.Infotel_address.visible = false;
@@ -225,20 +213,16 @@ class Action {
         that.scene.Infotel_phone.visible = true;
         that.scene.Infotel_email.visible = true;
 
-        that.scene.Infotel_chip_Home.texture = that.scene.Unclick_chip_Home;
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Infotel_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
+        that.scene.Infotel_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Infotel_chip_Aog.texture = that.scene.Unclick_chip_Aog;
         that.scene.Infotel_chip_Info.texture = that.scene.Unclick_chip_Info;
       },
 
       ESTABLISHDIV_INFO_OPTION: function(data) {
         console.log("command : " + data.command);
-
-//        that.scene.Hi_container.visible = false;
-//        that.scene.Fallback_container.visible = false;
-//        that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
-//        that.scene.Info_container.visible = false;
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Infotel_container.visible = true;
         that.scene.Infotel_prize.visible = false;
         that.scene.Infotel_address.visible = false;
@@ -252,6 +236,7 @@ class Action {
         that.scene.Infotel_phone.visible = false;
         that.scene.Infotel_email.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Infotel_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Infotel_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Infotel_chip_Aog.texture = that.scene.Unclick_chip_Aog;
@@ -260,11 +245,7 @@ class Action {
 
       PRIZEDIV_INFO_OPTION: function(data) {
         console.log("command : " + data.command);
-//        that.scene.Hi_container.visible = false;
-//        that.scene.Fallback_container.visible = false;
-//        that.scene.Greeting_container.visible = false;
-//        that.scene.Aog_container.visible = false;
-//        that.scene.Info_container.visible = false;
+        //해당 인텐트를 제외한 모든 인텐트의 화면을 숨김
         that.scene.Infotel_container.visible = true;
         that.scene.Infotel_prize.visible = true;
         that.scene.Infotel_address.visible = false;
@@ -278,6 +259,7 @@ class Action {
         that.scene.Infotel_phone.visible = false;
         that.scene.Infotel_email.visible = false;
 
+        //이미지가 변경된 버튼을 원래대로 변경
         that.scene.Infotel_chip_Home.texture = that.scene.Unclick_chip_Home;
         that.scene.Infotel_chip_Greeting.texture = that.scene.Unclick_chip_Greeting;
         that.scene.Infotel_chip_Aog.texture = that.scene.Unclick_chip_Aog;
